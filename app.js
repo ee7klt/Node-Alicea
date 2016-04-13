cd187ce471078d0c5a5649e509245150c22a8390
ee7klt <kltah1@gmail.com>
2016-04-13 11:32:22 +0800

    32. Object properties and functions in arrays * can access object properties with bracket notation * can loop over functions in arrays with forEach and pass those functions to the forEach handler (FCF)

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-var util = require('util');
+// 32. object properties and methods
 
-var name = 'Alex';
-var greeting = util.format('Hello, %s', name);
-util.log(greeting);
+
+var obj = {
+    greet: 'Hello'
+}
+
+console.log(obj.greet);
+console.log(obj['greet']);
+var prop = 'greet';  // not the property, but the variable that points to the string that is the property name.
+console.log(obj[prop]);
+
+// functions and arrays
+
+var arr = [];
+// functions sitting in array (uninvoked)
+arr.push( function() {
+    console.log('hello world 1');
+})
+arr.push( function() {
+    console.log('hello world 2');
+})
+arr.push( function() {
+    console.log('hello world 3');
+})
+
+arr.forEach(function (item) {
+    item();
+})
