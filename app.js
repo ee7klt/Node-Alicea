c6ec6ac6c776585a95b1758a7d6f6a234f3eb165
ee7klt <kltah1@gmail.com>
2016-04-19 14:24:37 +0800

    Pass data through to the emitter. * comes through the method on the prototype * and then on to the emit call from within the method

--- app.js
+++ app.js
@@ -11,21 +11,21 @@ function Greetr() {
 util.inherits(Greetr, EventEmitter);
 
 // can add my own properties later
-Greetr.prototype.greet = function() {
-    console.log(this.greeting);
+Greetr.prototype.greet = function(data1, data2) {
+    console.log(this.greeting + ': ' + data1+', '+data2);
     // can call methods from EventEmitter on 'this' because the 
     // created object has been connected via the newly created prototype chain 
     // to EventEmitter
-    this.emit('greet');
+    this.emit('greet', data1, data2);
 }
 
 
 
 var greeter1 = new Greetr();
-greeter1.on('greet', function() {
-    console.log('someone greeted');
+greeter1.on('greet', function(data1, data2) {
+    console.log('someone greeted: '+ data1+', '+ data2);
 });
 
-greeter1.greet();
+greeter1.greet('Alex','Jo');
 
 console.log(Greetr.prototype);
