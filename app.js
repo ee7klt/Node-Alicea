87322d7d1e167043761c377266ceda8d835a05b3
ee7klt <kltah1@gmail.com>
2016-04-19 13:18:16 +0800

    Inherit from the emitter using util.inherits * uses Object.setPrototypeOf

--- app.js
+++ app.js
@@ -1,45 +1,31 @@
+var EventEmitter = require('events');
+var util = require('util');
 
-// i want this to be the template for other objects i create
-
-var person = {
-    firstName: '',
-    lastName: '',
-    fedForehand: function() {
-        return this.firstName + ' ' + this.lastName + ': congratulations, you\'ve just been upgraded to the most elegant forehand available!'; 
-    }
+function Greetr() {
+    this.greeting = "Hello world!";
+    
 }
 
-// instead of putting this in a constructor, 
-// use object.create
+// any objects created by Greetr should have access to the prototype properties
+// of EventEmitter
+util.inherits(Greetr, EventEmitter);
+
+// can add my own properties later
+Greetr.prototype.greet = function() {
+    console.log(this.greeting);
+    // can call methods from EventEmitter on 'this' because the 
+    // created object has been connected via the newly created prototype chain 
+    // to EventEmitter
+    this.emit('greet');
+}
 
 
-// gives me an empty object whose prototype is person
-var alex = Object.create(person);
 
-// can overwrite them 
+var greeter1 = new Greetr();
+greeter1.on('greet', function() {
+    console.log('someone greeted');
+});
 
-alex.firstName = 'Alex'
-alex.lastName = 'Tah'
+greeter1.greet();
 
-// but if it doesn't find it defined, it'll go down the chain
-// like in this case it can't find fedForehand so it'll go down to find it in the prototype
-console.log(alex);
-console.log(alex.__proto__);
-console.log(person.prototype);  // undefined
-console.log(person);
-console.log(alex.__proto__ === person);   // true
-// it looks like person IS the prototype. 
-
-
-// this below returns
-// TypeError: Cannot set property 'djokerBackhand' of undefined
-// because there is no person.prototype.
-// person.prototype.djokerBackhand = function() {
-//     return this.firstName + ' ' + this.lastName + ': congratulations, you\'ve just been upgraded to the most versatile backhand available!';
-// }
-
-
-// so it looks like the difference between new and Object.create is that
-// new puts the constructor.prototype on to the newobject.__proto__
-// whereas Object.create puts the actual templateobject on to newobject.__proto__ (as opposed to templateobject.prototype)
-
+console.log(Greetr.prototype);
