4b58f2751b9c630fc26b05d785cb4d44941cfcc8
ee7klt <kltah1@gmail.com>
2016-04-19 11:57:56 +0800

    35 object.create

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
-var Emitter = require('events');
-var eventConfig = require('./config').events;
 
-var emtr = new Emitter();
+// i want this to be the template for other objects i create
 
-// i want to respond to an event 'affirm'
-// i'm calling 'affirm' an event, but its actually just a property name.
-// fake events!!
+var person = {
+    firstName: '',
+    lastName: '',
+    fedForehand: function() {
+        return this.firstName + ' ' + this.lastName + ': congratulations, you\'ve just been upgraded to the most elegant forehand available!'; 
+    }
+}
 
-// add responses (functions) that'll respond to the 'affirm' event.
-emtr.on(eventConfig.AFFIRM, function () {
-    console.log('I am a loveable human being');
-});
+// instead of putting this in a constructor, 
+// use object.create
 
-emtr.on(eventConfig.AFFIRM, function() {
-    console.log('What a beautiful world!');
-});
 
-// the affirmation
-console.log('Good morning!');
+// gives me an empty object whose prototype is person
+var alex = Object.create(person);
 
-// Hey, an affirmation happened, go tell the listeners!
-emtr.emit(eventConfig.AFFIRM);
+// can overwrite them 
+
+alex.firstName = 'Alex'
+alex.lastName = 'Tah'
+
+// but if it doesn't find it defined, it'll go down the chain
+// like in this case it can't find fedForehand so it'll go down to find it in the prototype
+console.log(alex);
+console.log(alex.__proto__);
+console.log(person.prototype);  // undefined
+console.log(person);
+console.log(alex.__proto__ === person);   // true
+// it looks like person IS the prototype. 
+
+
+// this below returns
+// TypeError: Cannot set property 'djokerBackhand' of undefined
+// because there is no person.prototype.
+// person.prototype.djokerBackhand = function() {
+//     return this.firstName + ' ' + this.lastName + ': congratulations, you\'ve just been upgraded to the most versatile backhand available!';
+// }
+
+
+// so it looks like the difference between new and Object.create is that
+// new puts the constructor.prototype on to the newobject.__proto__
+// whereas Object.create puts the actual templateobject on to newobject.__proto__ (as opposed to templateobject.prototype)
+
