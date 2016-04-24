ac4cf2179adcb813c0b6e12969368a948cc925dd
ee7klt <kltah1@gmail.com>
2016-04-24 19:18:51 +0800

    53 pipe. copy text using a pipe

--- app.pipe.js
+++ app.pipe.js
@@ -5,11 +5,7 @@ var fs = require('fs');
 //  @param : path = source path
 //  @param : options = an object of options
 var readable = fs.createReadStream(
-    __dirname + '/lorem.txt',
-    {
-        encoding:'utf8',
-        highWaterMark: 16*1024
-    }
+    __dirname + '/lorem.txt'
 );
 
 
@@ -17,18 +13,16 @@ var readable = fs.createReadStream(
 //  @param: path = write path
 var writable = fs.createWriteStream(__dirname + '/lorem.write.txt');
 
-// stream will fill a buffer
-// when file is completely read, 'data' event will be emitted by node
-// it will then run this listener i have defined here
 
-readable.on('data', function(chunk) {
-    console.log(chunk.length); // <Buffer 4c 6f 72 65 6d 20 69 70 73 75 6d 20 64 6f 6c 6f 72 20 73 69 74 20 61 6d 65 74 2c 20 63 6f 6e 73 65 63 74 65 74 75 72 20 61 64 69 70 69 73 63 69 6e 67 ... >
-    // on every chunk, send it to the write stream.
-    // will automatically write it to the file.
-    
-    writable.write(chunk);
 
-})
+// pipe
+//   @param: dest = destination to which to pipe the data coming from the readable stream
+//   @return: dest = so we can chain pipes.
+//   in the code 'src' is the readable stream.
+readable.pipe(writable);
+
+
+ 
 
 
 
