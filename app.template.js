30b9cf382afd48801609749330fa4c707be7ea70
ee7klt <kltah1@gmail.com>
2016-04-27 12:24:41 +0800

    61. streams and performance
    Stream the file instead of reading it wholesale
    * this will improve performace because we'll split the data in to chunks and stream each chunk (as opposed to the whole file)
    * whenever possible use streams

--- app.template.js
+++ app.template.js
@@ -2,8 +2,6 @@ var http = require('http');
 var fs = require('fs');
 http.createServer(function(req,res) {
     res.writeHead(200, {'Content-Type': 'text/html' })
-    var html = fs.readFileSync(__dirname +'/index.htm','utf-8');
-    var mensaje = 'mi debes dinero';
-    html = html.replace('{mensaje}', mensaje);
-    res.end(html);
+    fs.createReadStream(__dirname +'/index.htm','utf-8').pipe(res,{'encoding':'utf-8'});
+   
 }).listen(1337,'127.0.0.1');
