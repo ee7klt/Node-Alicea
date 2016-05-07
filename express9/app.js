03024f58035c3ee2cf72f6e7ff5d290e5f4f95de
ee7klt <kltah1@gmail.com>
2016-05-07 14:21:35 +0800

    73. express
    * express is a collection of shortcuts to commonly required server stuff of the node core
    * requiring express will return a function called app
    * on app is many properties and functions
    * like app.get that'll respond to HTTP GET method etc.

---
+++ express9/app.js
@@ -0,0 +1,25 @@
+// will look in node_modules for express since it's not built in to node
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+
+// when a HTTP GET method comes in
+// to url /
+// run this callback with the req and res passed to it
+
+app.get('/', function(req, res) {
+    res.send('<html><body>  <h2> This is the express train to JS! </h2> </body>  </html>'
+    )
+})
+
+app.get('/api', function(req, res){
+    res.json({
+        'dessert': 'warabimochi',
+        'origin': 'kyoto'
+    })
+})
+
+app.listen(3000);
+
+
