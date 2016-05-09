b3b22850f2c19bb41c89e1087cd121036abd4826
ee7klt <kltah1@gmail.com>
2016-05-09 10:28:46 +0800

    75. static files and middleware
    * middleware in express is code that sits between the request and the response
    * static is a built-in middleware that helps serve static assets like img css

--- express9/app.js
+++ express9/app.js
@@ -3,13 +3,20 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+// app.use(function (req, res, next) {
+//   console.log('Time:', Date.now());
+//   next();
+// })
+
+app.use('/assets', express.static(__dirname + '/public') );
+
 
 // when a HTTP GET method comes in
 // to url /
 // run this callback with the req and res passed to it
 
 app.get('/', function(req, res) {
-    res.send('<html><body>  <h2> This is the express train to JS! </h2> </body>  </html>'
+    res.send('<html><body><head><link href=assets/style.css type=text/css rel=stylesheet><link/></head>  <h2> This is the express train to JS! </h2><img src=assets/file.jpeg /> </body>  </html>'
     )
 });
 
@@ -18,12 +25,20 @@ app.get('/kyoto/:id', function(req,res) {
     )
 });
 
-app.get('/api', function(req, res){
+app.get('/api', function(req, res,next){
     res.json({
         'dessert': 'warabimochi',
         'origin': 'kyoto'
     })
-})
+    next();
+},
+    function(req,res,next){
+        console.log(`this is api`);
+    }
+);
+
+
+
 
 app.listen(3000);
 
