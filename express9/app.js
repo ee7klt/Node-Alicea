bc6cfcfe5d654650f48dfe548ec83b3bc1ed846b
ee7klt <kltah1@gmail.com>
2016-05-09 12:11:04 +0800

    76 template engines
    * use app.set('view engine', ..)  to select a particular view engine to use
    
    * after creating the view file *.ejs, use res.render(FILENAME) without extensions, instead of res.send
    * to have the stylesheet load properly for dynamically generated url, use absolute path /assets/style.css instead of relative path assets/style.css

--- express9/app.js
+++ express9/app.js
@@ -3,26 +3,37 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+// use ejs.co as the template engine
+app.set('view engine', 'ejs');
+
+
+
 // app.use(function (req, res, next) {
 //   console.log('Time:', Date.now());
 //   next();
 // })
 
+// mount express.static middleware on to the /assets path
+// when request comes in for any files in assets folder, serve it up using express.static
 app.use('/assets', express.static(__dirname + '/public') );
+app.use('/', function(req,res,next) {
+    console.log('Request url: ' + req.url);
+    console.log('__dirname is '+ __dirname);
+  next();
+});
+
 
 
 // when a HTTP GET method comes in
 // to url /
 // run this callback with the req and res passed to it
-
+// request for css from assets
 app.get('/', function(req, res) {
-    res.send('<html><body><head><link href=assets/style.css type=text/css rel=stylesheet><link/></head>  <h2> This is the express train to JS! </h2><img src=assets/file.jpeg /> </body>  </html>'
-    )
+    res.render('index'); // automatically inserts .ejs from app.set
 });
 
-app.get('/kyoto/:id', function(req,res) {
-     res.send(`<html><body><h2> Vamos ${req.params.id}-chan! </h2></body></html>`
-    )
+app.get('/kyoto/:site', function(req,res) {
+    res.render('sites', {SITE: req.params.site});
 });
 
 app.get('/api', function(req, res,next){
