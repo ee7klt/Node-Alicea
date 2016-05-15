d3f6197d7540b6af6298d94bf94f703284573696
ee7klt <kltah1@gmail.com>
2016-05-15 14:25:28 +0800

    88 angularjs part 3: manipulating the DOM
    * directive = attribute or attribute value in html element
    * when html is processed by browser, these directives will tell angular to do stuff
    * here it will replace the {{vm.message}} placeholder with the this.message defined in /public/js/app.js
    * this === vm in /public/js/app.js because we said 'as vm'

--- mean/public/js/app.js
+++ mean/public/js/app.js
@@ -1,1 +1,21 @@
 // js code downloaded by browser (client)
+// angular.js would already have been loaded in the head tag of index.ejs
+
+// set up new module (my angular app)
+// similar idea to module.exports (self-contained, no collisions)
+// empty array is list of modules that this module depends on (kinda like require)
+angular.module('TestApp', []);
+
+// now to do other things on TestApp
+// add controller of name 'MainController' to TestApp
+// and execute ctrlFunc
+angular.module('TestApp')
+    .controller('MainController', ctrlFunc);
+    
+    
+function ctrlFunc() {
+    // 'this' will point to vm in index.ejs because i said 'as vm'
+    // vm = view model = model that i want for this particular screen
+    this.message = 'Hello';
+    // now messaged is attached to vm and can be accessed from index.
+}
